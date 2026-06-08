0a20e9e414694c051be71365480aa85d5750d00b
vv-glitchhub <vikke.vuorio99@gmail.com>
2026-06-08 13:34:46 +0300

    Add signal flow pulses to neural hero

--- components/NeuralHero.tsx
+++ components/NeuralHero.tsx
@@ -20,10 +20,32 @@ const lines = [
   [6, 7], [7, 8], [8, 9], [2, 7], [7, 10], [6, 11], [8, 10], [7, 11]
 ]
 
+const flows = [
+  [0, 1, 2, 3, 4, 5],
+  [9, 8, 7, 6, 11],
+  [2, 7, 10],
+  [4, 5, 6, 7, 8],
+]
+
+function pointsToPath(path: number[]) {
+  return path
+    .map((nodeIndex, index) => {
+      const node = nodes[nodeIndex]
+      return `${index === 0 ? 'M' : 'L'} ${node.x} ${node.y}`
+    })
+    .join(' ')
+}
+
 export default function NeuralHero() {
   return (
     <div className="neural-hero" aria-hidden="true">
       <svg viewBox="0 0 100 100" preserveAspectRatio="none" className="neural-svg">
+        <defs>
+          {flows.map((flow, index) => (
+            <path key={`flow-path-${index}`} id={`flow-path-${index}`} d={pointsToPath(flow)} />
+          ))}
+        </defs>
+
         {lines.map(([a, b], index) => (
           <line
             key={`${a}-${b}`}
@@ -35,6 +57,19 @@ export default function NeuralHero() {
             style={{ animationDelay: `${index * 0.18}s` }}
           />
         ))}
+
+        {flows.map((_, index) => (
+          <circle key={`signal-${index}`} r="1.05" className="neural-signal">
+            <animateMotion
+              dur={`${5.2 + index * 0.8}s`}
+              repeatCount="indefinite"
+              begin={`${index * 0.7}s`}
+            >
+              <mpath href={`#flow-path-${index}`} />
+            </animateMotion>
+          </circle>
+        ))}
+
         {nodes.map((node, index) => (
           <circle
             key={`${node.x}-${node.y}`}
