ef9e63192118d2d4601a5539fd64622a04ca40cd
vv-glitchhub <vikke.vuorio99@gmail.com>
2026-06-24 14:23:57 +0300

    Harden production Next config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  poweredByHeader: false,
+  reactStrictMode: true,
+}
 
 export default nextConfig
